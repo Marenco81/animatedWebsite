fb6190347ca468c2243b3f9dd13c73190a83793a
Alvaro Marenco <alvaromarencom@gmail.com>
2025-09-01 12:25:31 -0600

    Loading animation

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const smoother = ScrollSmoother.create({
     effects: true,
 });
 
+const tl = gsap.timeline();
+
+tl.to(".loading", {
+    width: "100%",
+    duration: 0.9,
+    delay: 0.3,
+})
+.to(".capsuleLogo", {
+    scale: 2,
+    duration: 0.5,
+    transition: "0.1s ease",
+    opacity: 0,
+}, "a")
+.to(".capsule", {
+    clipPath: "inset(0% 0% 0% 0%)",
+}, "a")
+
 const tl1 = gsap.timeline({
     scrollTrigger: {
         trigger: ".page1",
         start: "90% 80%",
         end: "155% 80%",
-        markers: true,
         scrub: true,
     }    
 });
